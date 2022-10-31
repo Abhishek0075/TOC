5341ff758bc1004bcb36420a62268d4ec0cf4f1a
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-10-31 23:33:03 +0530

    Email problem solved

--- email.java
+++ email.java
@@ -1,29 +1,88 @@
 class email{
     public static void email_verify(String mail){
-        boolean q0,q1,q2,q3,q4,qd;
-        q0=q1=q2=q3=q4=qd=false; 
+        boolean beforeAt,At,afterAt,dot,dotAfter,qd;
+        beforeAt=At=afterAt=dot=dotAfter=qd=false;
         int mover=0;
-        if(mail.charAt(mover)=='@' || mail.charAt(mover)=='.'){
-            System.out.println("Death State reached !!");
+        if (!Character.isDigit(mail.charAt(mover))
+            && !Character.isLetter(mail.charAt(mover))
+            && !Character.isWhitespace(mail.charAt(mover))) {
+                qd=true;
+                System.out.println("Death state reached");
+                return;
+        }else{
+            beforeAt=true;
+            mover+=1;
+        }
+        //At symbol
+        while(qd==false && At==false && mover<mail.length()){
+            if(mail.charAt(mover)=='@'){
+                At=true;
+                mover+=1;
+                break;
+            }
+            if (!Character.isDigit(mail.charAt(mover))
+            && !Character.isLetter(mail.charAt(mover))
+            && !Character.isWhitespace(mail.charAt(mover))) {
+                if(mail.charAt(mover)=='_'){
+                }else{
+                    qd=true;
+                    System.out.println("Death state reached");
+                    return;
+                }
+            }
+            mover+=1;
+        }
+        //After at
+        if (qd==false && Character.isLetter(mail.charAt(mover))) {
+            afterAt=true;
+            mover+=1;
+        }else{
+            qd=true;
+            System.out.println("Death state reached");
             return;
         }
-        while(q1==false && mover<mail.length()){
+        //dot
+        while(qd==false && dot==false && mover<mail.length()){
             if(mail.charAt(mover)=='@'){
-                q1=true;
+                System.out.println("Death State reached");
+                qd=true;
+                return;
+            }
+            if(mail.charAt(mover)=='.'){
+                dot=true;
                 mover+=1;
                 break;
             }
             mover+=1;
         }
-        while(q2==false && mover<mail.length()){
-            if(mail.charAt(mover)=='@'){
-                System.out.println("Death State reached !!");
-                return;
-            }
-            if(mail.charAt(mover)=='.'){
-                q2=true;
+        //dotAfter
+        char value=mail.charAt(mover);
+        Boolean checker=Character.isLetter(mail.charAt(mover));
+        if(qd==false && Character.isLetter(mail.charAt(mover))){
+            dotAfter=true;
+            mover+=1;
+        }
+        while(qd==false&& mover<mail.length()){
+            if(Character.isDigit(mail.charAt(mover))
+                && Character.isWhitespace(mail.charAt(mover))){
+                    qd=true;
+                    System.out.println("Death state reached");
+                    return;
+            }else if(!Character.isLetter(mail.charAt(mover))
+                && !Character.isDigit(mail.charAt(mover))
+                && !Character.isWhitespace(mail.charAt(mover))){
+                    qd=true;
+                    System.out.println("Death state reached");
+                    return;
             }
             mover+=1;
         }
+        if(qd==false && beforeAt==true && At==true && afterAt==true && dot==true && dotAfter==true){
+            System.out.println("The system is a DFA");
+        }
+    }
+    public static void main(String[] args) {
+        String mail="@adad@@gmail.com";
+        email_verify(mail);
     }
 }
--- test.java
+++ test.java
@@ -1,6 +1,15 @@
 public class test {
     public static void main(String[] args) {
-        String a="com";
+        String s="argsAAA@$)";
+        int  count=0;
+        for(int i=0;i<s.length();i++){
+        if (!Character.isDigit(s.charAt(i))
+            && !Character.isLetter(s.charAt(i))
+            && !Character.isWhitespace(s.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println("The number of special characters in "+s+" is "+count);
         return;
     }
 }
